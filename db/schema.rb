3b3841aa21e757113ab795af3ee7fdd182795734
raissa santana <raissasilmesant@gmail.com>
2020-09-18 16:33:37 -0300

    Prepare rails app with external specs

---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_09_18_183444) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cocktails", force: :cascade do |t|
+    t.string "index"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+  end
+
+  create_table "doses", force: :cascade do |t|
+    t.bigint "cocktail"
+    t.bigint "ingredient"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
+    t.index ["cocktail"], name: "index_doses_on_cocktail"
+    t.index ["ingredient"], name: "index_doses_on_ingredient"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+  end
+
+  add_foreign_key "doses", "cocktails", column: "cocktail"
+  add_foreign_key "doses", "ingredients", column: "ingredient"
+end
